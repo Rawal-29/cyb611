a5b21b24cfeda46c8bd7feb036947bd941c66047
Rawal <rawalsandhan2909@gmail.com>
2025-11-21 09:44:14 -0500

    Changes to Fix insecure bucket

--- insecure_rawal.tf
+++ insecure_rawal.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "insecure_bucket" {
-  bucket = "cyb611-secure-phish-bits-insecure_bucket_001"
+  bucket = "cyb611-secure-phish-bits-insecure-bucket-001"
   
   tags = {
     Name        = "CYB611 InSecure"
