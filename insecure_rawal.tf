0c6f24ef994f5a2da431ed86b6e52806ec6af4e0
sravaniappari <sravaniappari156@gmail.com>
2025-11-21 11:11:09 -0500

    Update insecure_rawal.tf

--- insecure_rawal.tf
+++ insecure_rawal.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "insecure_bucket" {
   bucket = "cyb611-secure-phish-bits-insecure-bucket-001"
-  
+
+
   tags = {
     Name        = "CYB611 InSecure"
     Environment = "Test"
