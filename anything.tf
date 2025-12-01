c8cb00205ba6ed3584bba320db173cbb2d1d5641
Erica Achiaa <erica.achiaa21@gmail.com>
2025-12-01 06:15:02 -0500

    Adding a public R+W bucket

--- anything.tf
+++ anything.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "anything" {
-  bucket = "cyb611-phish-bits-insecure-ea-public-access"
+resource "aws_s3_bucket" "public_s3_rw_access" {
+  bucket = "cyb611-phish-bits-insecure-ea-s3-public-write"
   
   tags = {
     Name        = "CYB611 InSecure-EA-public-access"
@@ -8,11 +8,27 @@ resource "aws_s3_bucket" "anything" {
 }
 
 
-# [EXISTING BLOCKS KEPT THE SAME]
-resource "aws_s3_bucket_public_access_block" "block2" {
-  bucket = aws_s3_bucket.anything.id
-  block_public_acls       = false
+# [Public Read-Write Access]
+resource "aws_s3_bucket_public_access_block" "public_rw_disable" {
+  bucket = aws_s3_bucket.public_s3_rw_access.id
+
+  block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_rw_policy" {
+  bucket = aws_s3_bucket.public_s3_rw_access.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicWrite"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = ["s3:PutObject", "s3:PutObjectAcl"]
+      Resource  = "${aws_s3_bucket.public_s3_rw_access.arn}/*"
+    }]
+  })
 }
