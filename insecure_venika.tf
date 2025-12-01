0f7ec83b9c8673984c280c7c18cb6211320c4018
Venika <77220620+venikajarvis@users.noreply.github.com>
2025-12-01 11:59:16 -0500

    insecure_venika.tf

--- insecure_venika.tf
+++ insecure_venika.tf
@@ -1,1 +1,19 @@
+resource "aws_s3_bucket_server_side_encryption_configuration" "weak" {
+  bucket = aws_s3_bucket.b.id
 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure" {
+  bucket = aws_s3_bucket.b.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
