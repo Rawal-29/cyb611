fb0ed91c5bb1ca004ed7dc18da5515f8aa3b37f4
Rawal <rawalsandhan2909@gmail.com>
2025-12-09 15:56:57 -0500

    Changes

--- infrastructure/iam_misconfig.tf
+++ infrastructure/iam_misconfig.tf
@@ -1,73 +1,79 @@
-resource "random_string" "iam_id" {
+resource "random_string" "dev_id" {
   length  = 6
   special = false
   upper   = false
 }
 
-
-resource "aws_s3_bucket" "iam_logs" {
-  bucket = "cyb611-insecure-iam-logs-${random_string.iam_id.result}"
+resource "aws_s3_bucket" "dev_logs" {
+  bucket = "cyb611-insecure-iam-logs-${random_string.dev_id.result}"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "iam_log_ownership" {
-  bucket = aws_s3_bucket.iam_logs.id
+resource "aws_s3_bucket_ownership_controls" "dev_log_ownership" {
+  bucket = aws_s3_bucket.dev_logs.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "iam_log_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.iam_log_ownership]
-  bucket     = aws_s3_bucket.iam_logs.id
+resource "aws_s3_bucket_acl" "dev_log_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.dev_log_ownership]
+  bucket     = aws_s3_bucket.dev_logs.id
   acl        = "log-delivery-write"
 }
 
-
-resource "aws_s3_bucket" "iam_bucket" {
-  bucket        = "cyb611-insecure-iam-${random_string.iam_id.result}"   
+resource "aws_s3_bucket" "dev_bucket" {
+  bucket        = "cyb611-insecure-iam-${random_string.dev_id.result}"   
   force_destroy = true
+
   tags = {
     Name = "Insecure IAM"
   }
 }
 
 
-resource "aws_s3_bucket_public_access_block" "iam_unsafe" {
-  bucket = aws_s3_bucket.iam_bucket.id
+resource "aws_s3_bucket_public_access_block" "dev_unsafe" {
+  bucket = aws_s3_bucket.dev_bucket.id
   block_public_acls       = true
-  block_public_policy     = false # FAIL
+  block_public_policy     = false 
   ignore_public_acls      = true
-  restrict_public_buckets = false # FAIL
+  restrict_public_buckets = false 
 }
 
-# SECURE: Versioning Enabled (Blocks Ransomware)
-resource "aws_s3_bucket_versioning" "iam_versioning" {
-  bucket = aws_s3_bucket.iam_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-
-resource "aws_s3_bucket_logging" "iam_logging" {
-  bucket        = aws_s3_bucket.iam_bucket.id
-  target_bucket = aws_s3_bucket.iam_logs.id
-  target_prefix = "log/"
-}
-
-
-resource "aws_s3_bucket_ownership_controls" "iam_ownership" {
-  bucket = aws_s3_bucket.iam_bucket.id
+resource "aws_s3_bucket_ownership_controls" "dev_ownership" {
+  bucket = aws_s3_bucket.dev_bucket.id
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "dev_enc" {
+  bucket = aws_s3_bucket.dev_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-resource "aws_s3_bucket_policy" "iam_policy" {
-  bucket = aws_s3_bucket.iam_bucket.id
-  depends_on = [aws_s3_bucket_public_access_block.iam_unsafe]
+
+resource "aws_s3_bucket_versioning" "dev_versioning" {
+  bucket = aws_s3_bucket.dev_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "dev_logging" {
+  bucket        = aws_s3_bucket.dev_bucket.id
+  target_bucket = aws_s3_bucket.dev_logs.id
+  target_prefix = "log/"
+}
+
+
+resource "aws_s3_bucket_policy" "dev_policy" {
+  bucket = aws_s3_bucket.dev_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.dev_unsafe]
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -78,16 +84,16 @@ resource "aws_s3_bucket_policy" "iam_policy" {
         Principal = "*",
         Action    = "s3:*",
         Resource  = [
-          "${aws_s3_bucket.iam_bucket.arn}",
-          "${aws_s3_bucket.iam_bucket.arn}/*"
+          "${aws_s3_bucket.dev_bucket.arn}",
+          "${aws_s3_bucket.dev_bucket.arn}/*"
         ]
       }
     ]
   })
 }
 
-resource "aws_s3_object" "iam_file" {
-  bucket       = aws_s3_bucket.iam_bucket.id
+resource "aws_s3_object" "iam_config" {
+  bucket       = aws_s3_bucket.dev_bucket.id
   key          = "sensitive_data/mock_pii.csv"
   content_type = "text/csv"
   content      = "id,secret\n1,DevData"
